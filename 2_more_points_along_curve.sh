723f325edca16c520243288be5bcb0f39988e417
Ethan Caballero <ethanvictorcaballero@gmail.com>
2022-09-13 17:32:23 -0400

    Update 2_more_points_along_curve.sh

--- 2_more_points_along_curve.sh
+++ 2_more_points_along_curve.sh
@@ -7,9 +7,9 @@ do
 
    size576=900
    seed576=$(expr $pre + $size576)
-   sbatch --time=0:30:0 --gres=gpu:1 job_cc_cedar.sh --data_steps=750000 --seed=$seed576 --train_set_size=576 --batch_size_train=576 --wandb_tag=math__d_axis__digits4__lr0001 --iters_per_eval=16 --test_set_size=10000 --n_digit=4 --lr=.0001 --dropout_prob=0.0
+   sbatch --time=0:35:0 --gres=gpu:1 job_cc_cedar.sh --data_steps=750000 --seed=$seed576 --train_set_size=576 --batch_size_train=576 --wandb_tag=math__d_axis__digits4__lr0001 --iters_per_eval=16 --test_set_size=10000 --n_digit=4 --lr=.0001 --dropout_prob=0.0
 
    size608=1101
    seed608=$(expr $pre + $size608)
-   sbatch --time=0:30:0 --gres=gpu:1 job_cc_cedar.sh --data_steps=750000 --seed=$seed608 --train_set_size=608 --batch_size_train=608 --wandb_tag=math__d_axis__digits4__lr0001 --iters_per_eval=16 --test_set_size=10000 --n_digit=4 --lr=.0001 --dropout_prob=0.0
+   sbatch --time=0:35:0 --gres=gpu:1 job_cc_cedar.sh --data_steps=750000 --seed=$seed608 --train_set_size=608 --batch_size_train=608 --wandb_tag=math__d_axis__digits4__lr0001 --iters_per_eval=16 --test_set_size=10000 --n_digit=4 --lr=.0001 --dropout_prob=0.0
 done
